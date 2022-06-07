74134ca7d1b35ef9f14c4ba15571c44cf31415fc
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2022-06-07 17:37:26 +0200

    feature: simple create post test

--- supertest/post.st.js
+++ supertest/post.st.js
@@ -13,7 +13,19 @@ describe('Creating Post', () =>{
 
     })
 
-    it('Create empty post', async ()=>{
-        console.log(token)
+    it('Create empty post', async (done )=>{
+        request.post('/wp-json/wp/v2/posts')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                title: 'test-fromapi-commited'
+            })
+            .expect(201)
+            .then((response)=>{
+                expect(response.body.status).to.equal('draft');
+                expect(response.body.title.raw).to.equal('test-fromapi-commited');
+
+                done();
+            })
+            .catch((err) => done(err));
     })
 })
